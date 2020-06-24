f28264aa6b45193eb818004cecec6e180b19beca
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2020-06-24 12:26:35 +0300

    add interfaces

--- contracts/interfaces/liquidity/ILiquidityModule.sol
+++ contracts/interfaces/liquidity/ILiquidityModule.sol
@@ -12,20 +12,13 @@ interface ILiquidityModule {
     /*
      * @notice Deposit amount of lToken and mint pTokens
      * @param lAmount Amount of liquid tokens to invest
-     * @param pAmountMin Minimal amout of pTokens suitable for sender
      */ 
-    function deposit(uint256 lAmount, uint256 pAmountMin) external;
+    function deposit(address token, uint256 dnlAmount) external;
 
     /**
      * @notice Withdraw amount of lToken and burn pTokens
-     * @param pAmount Amount of pTokens to send
      * @param lAmountMin Minimal amount of liquid tokens to withdraw
      */
-    function withdraw(uint256 pAmount, uint256 lAmountMin) external;
+    function withdraw(uint256 lAmount, address token) external;
 
-    /**
-     * @notice Simulate withdrawal for loan repay with PTK
-     * @param pAmount Amount of pTokens to use
-     */
-    function withdrawForRepay(address borrower, uint256 pAmount) external;
 }
