25ab466e8e4f3df2e421eeefa962c85f6449dc07
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2020-07-17 16:16:47 +0300

    IDCAToken Interface

---
+++ contracts/interfaces/dca/IDCAToken.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.5.12;
+
+interface IDCAToken {
+
+    //Adress for Token as Currency for Buy. Default: WBTC
+    address internal constant WBTC_TOKEN_ADDRESS = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2;
+
+
+    //Adress for Token as Currency of Deposit. Default: USDC
+    address internal constant STACKING_TOKEN_ADDRESS = 0xCFd6e4044DD6E6CE64AeD0711F849C7B9134d7Db;
+
+
+    event Supply( 
+        uint256 value, 
+        uint256 tokenAddress,
+        uint256 indexed tokenId,
+    );
+
+
+    event DCAStrategyUpdated(
+        uint256 indexed tokenId,
+        uint256 duration,
+        uint256 minAmount
+    );
+
+
+    event DCAStrategyRemoved(
+        uint256 indexed tokenId
+    );
+
+    event Redeem(
+        uint256 value, 
+        uint256 tokenAddress,
+        uint256 indexed tokenId,
+    );
+
+
+
+    function balanceOf(address token) external view returns
+    (
+        uint256 balance
+    );
+
+    function dcaFor(address account) external view returns
+    (
+        uint256 tokenId;
+    );
+
+
+    function dcaStrategyFor(address account) external view returns
+    (
+        uint256 tokenId;
+    );
+
+
+    function supply(
+        uint256 value    
+    )  external returns (bool);
+
+
+     function  withdraw(
+        address token,
+        uint256 value    
+    )  external returns (bool);
+
+}
