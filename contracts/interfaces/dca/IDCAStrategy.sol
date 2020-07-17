441b139916bc7af39b206ca72664eacd559cd136
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2020-07-17 16:31:55 +0300

    exchange function

--- contracts/interfaces/dca/IDCAStrategy.sol
+++ contracts/interfaces/dca/IDCAStrategy.sol
@@ -2,6 +2,14 @@ pragma solidity ^0.5.12;
 
 interface IDCAStrategy {
 
+
+    //Adress for Token as Currency for Buy. Default: WBTC
+    address internal constant WBTC_TOKEN_ADDRESS = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2;
+
+
+    //Adress for Token as Currency of Deposit. Default: USDC
+    address internal constant STACKING_TOKEN_ADDRESS = 0xCFd6e4044DD6E6CE64AeD0711F849C7B9134d7Db;
+
     event DCAStrategyUpdated(
         uint256 indexed tokenId,
         uint256 duration,
@@ -13,6 +21,13 @@ interface IDCAStrategy {
         uint256 indexed tokenId
     );
 
+    event Exchange(
+        address indexed tokenFrom,
+        address indexed tokenTo,
+        uint256 amount1,
+        uint256 amount2
+    )
+
     function dcaStrategyFor(address account) external view returns
     (
         uint256 tokenId;
@@ -20,22 +35,18 @@ interface IDCAStrategy {
 
     function createDCAStrategy(uint256 indexed tokenId,
         uint256 duration,
-        uint256 minAmount) external view returns
+        uint256 minAmount) external returns
     (
         bool
     );
+   
 
-
-    function createDCAStrategy(uint256 indexed tokenId,
-        uint256 duration,
-        uint256 minAmount) external view returns
+    function removeDCAStrategy(uint256 indexed tokenId) external returns
     (
         bool
     );
-
-
-    function removeDCAStrategy(uint256 indexed tokenId) external view returns
-    (
-        bool
-    );
+ 
+    function exchange() external returns (
+        bool;
+    )
 }
--- contracts/interfaces/dca/IDCAToken.sol
+++ contracts/interfaces/dca/IDCAToken.sol
@@ -2,13 +2,6 @@ pragma solidity ^0.5.12;
 
 interface IDCAToken {
 
-    //Adress for Token as Currency for Buy. Default: WBTC
-    address internal constant WBTC_TOKEN_ADDRESS = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2;
-
-
-    //Adress for Token as Currency of Deposit. Default: USDC
-    address internal constant STACKING_TOKEN_ADDRESS = 0xCFd6e4044DD6E6CE64AeD0711F849C7B9134d7Db;
-
 
     event Supply( 
         uint256 value, 
@@ -23,6 +16,11 @@ interface IDCAToken {
     );
 
 
+
+    function totalSupplyFor(address token) external view returns
+    (
+        uint256 totalSupply
+    );
 
     function balanceOf(address token) external view returns
     (
@@ -41,7 +39,7 @@ interface IDCAToken {
 
      function  withdraw(
         address token,
-        uint256 value    
+        uint256 amount   
     )  external returns (bool);
 
 }
