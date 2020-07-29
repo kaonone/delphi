ede960ec5718691b869da3b185f7b8d3bc24997e
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2020-07-29 17:08:56 +0300

    lost contacts

---
+++ contracts/deploy/CompoundProtocol_DAI.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.12;
+
+import "../modules/defi/CompoundProtocol.sol";
+
+contract CompoundProtocol_DAI is CompoundProtocol {
+    function initialize(address _pool, address _token, address _cToken, address _comptroller) public initializer {
+        CompoundProtocol.initialize(
+            _pool, 
+            _token,
+            _cToken,
+            _comptroller
+        );
+    }    
+}
