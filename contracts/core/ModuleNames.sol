a05d7b94e323db12189deef03f5719e930e3b790
Pavel Rubin <pash7ka@gmail.com>
2020-08-10 12:03:59 +0300

    Update module names

--- contracts/core/ModuleNames.sol
+++ contracts/core/ModuleNames.sol
@@ -7,9 +7,8 @@ contract ModuleNames {
     // Pool Modules
     string internal constant MODULE_ACCESS            = "access";
     string internal constant MODULE_SAVINGS           = "savings";
-
+    string internal constant MODULE_INVESTING         = "investing";
     string internal constant MODULE_STAKING           = "staking";
-
     string internal constant MODULE_DCA               = "dca";
 
     // External Modules (used to store addresses of external contracts)
