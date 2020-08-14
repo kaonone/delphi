8cea700703dfa312e124dbae02ae3d510d941c32
Pavel Rubin <pash7ka@gmail.com>
2020-08-14 13:29:38 +0300

    Compound reward claiming optimization

--- contracts/interfaces/defi/IComptroller.sol
+++ contracts/interfaces/defi/IComptroller.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.16;
 
-contract IComptroller {
-    function claimComp(address holder) public;
-    function getCompAddress() public view returns (address);
+interface IComptroller {
+    function claimComp(address holder) external;
+    function claimComp(address[] calldata holders, address[] calldata cTokens, bool borrowers, bool suppliers) external;
+    function getCompAddress() external view returns (address);
 }
