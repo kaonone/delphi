07374e33c0f57f471262c7b9a7a4ea92bcc34ac4
Pavel Rubin <pash7ka@gmail.com>
2020-09-08 12:54:10 +0300

    Fix wrong module checked when burning

--- contracts/deploy/PoolToken_CurveFi_SBTC.sol
+++ contracts/deploy/PoolToken_CurveFi_SBTC.sol
@@ -12,6 +12,16 @@ contract PoolToken_CurveFi_SBTC is PoolToken {
         );
     }
 
+    function burnFrom(address from, uint256 value) public {
+        address investingModule = getModuleAddress(MODULE_INVESTING);
+        if (_msgSender() == investingModule) {
+            //Skip decrease allowance
+            _burn(from, value);
+        }else{
+            super.burnFrom(from, value);
+        }
+    }
+
     function userBalanceChanged(address account) internal {
         IPoolTokenBalanceChangeRecipient investing = IPoolTokenBalanceChangeRecipient(getModuleAddress(MODULE_INVESTING));
         investing.poolTokenBalanceChanged(account);
