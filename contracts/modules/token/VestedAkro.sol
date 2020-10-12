608f8a9b44d5c2c6837deb33b15d27c02683825e
Pavel Rubin <pash7ka@gmail.com>
2020-10-12 13:33:27 +0300

    wip

--- contracts/modules/token/VestedAkro.sol
+++ contracts/modules/token/VestedAkro.sol
@@ -1,8 +1,45 @@
 pragma solidity ^0.5.12;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/GSN/Context.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 
 
-contract VestedAkro {
+
+
+contract VestedAkro is Context, Initializable, Ownable, IERC20, ERC20Detailed {
+    struct VestedBatch {
+        uint256 amount;     // Full amount of AKRO vested in this batch
+        uint256 start;      // Vesting start time;
+        uint256 end;        // Vesting end time
+        uint256 claimed;    // AKRO already claimed from this batch
+    }
+
+    struct Holder {
+        VestedBatch[] batches;
+    }
+
+
+    IERC20 public akro;
+    mapping(address => Holder) holders;
+
+
+    function initialize(address _akro) public initializer {
+        Ownable.initialize(_msgSender());
+        ERC20Detailed.initialize("Vested AKRO", "vAKRO", 18);
+        akro = IERC20(_akro);
+    }
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+
+
 }
