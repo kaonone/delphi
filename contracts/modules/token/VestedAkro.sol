6afb3f61e87ae80cfe58603af83adc112cfd5d4d
Pavel Rubin <pash7ka@gmail.com>
2020-10-12 10:59:46 +0300

    Add VestedAkro

---
+++ contracts/modules/token/VestedAkro.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.12;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
+
+
+contract VestedAkro {
+}
