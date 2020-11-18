afea44dd784e5b9bb2539931c92d6b26a47a0ea2
Pavel-Blaize <pavel.g@blaize.tech>
2020-11-18 09:35:53 +0200

    Slither integration. Action correction

--- security/slither/run-slither-check.sh
+++ security/slither/run-slither-check.sh
@@ -2,4 +2,4 @@ FALSE_POSITIVE_WARNIGNS=0
 FOUND_WARNINGS=`grep "\d+ result" -Po output/slither_report.txt | sed 's/ result//g'`
 
 echo "Slither has found $FOUND_WARNINGS issues"
-if [[ $FOUND_WARNINGS -gt $FALSE_POSITIVE_WARNIGNS ]]; then echo "$FOUND_WARNINGS issues exceeds limit" >&2; exit 1; fi
+if [ $FOUND_WARNINGS -gt $FALSE_POSITIVE_WARNIGNS ]; then echo "$FOUND_WARNINGS issues exceeds limit" >&2; exit 1; fi
