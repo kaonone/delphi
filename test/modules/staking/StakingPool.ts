b02ace57ad7b575e12fcb31feccd6db184463b79
Pavel Rubin <pash7ka@gmail.com>
2020-11-19 14:57:47 +0300

    testing (wip)

--- test/modules/staking/StakingPool.ts
+++ test/modules/staking/StakingPool.ts
@@ -22,6 +22,7 @@ const StakingPool = artifacts.require("StakingPool");
 contract("StakingPool", async ([owner, user, ...otherAccounts]) => {
     const ZERO_BN = new BN("0");
     const ZERO_DATA = "0x";
+    const REWARD_EPOCH_DURATION = 24*60*60;
 
     let snap: Snapshot;
     let akro: FreeErc20Instance;
@@ -41,7 +42,6 @@ contract("StakingPool", async ([owner, user, ...otherAccounts]) => {
         await pool.methods['initialize()']();
 
         stakingPool = await StakingPool.new();
-
         await stakingPool.methods['initialize(address,address,uint256)'](pool.address, akro.address, 0);
 
         rewardVestingModule = await RewardVestingModule.new();
@@ -52,13 +52,29 @@ contract("StakingPool", async ([owner, user, ...otherAccounts]) => {
         await pool.set("adel", adel.address, false);
         await pool.set("staking", stakingPool.address, false);
 
+        await stakingPool.setRewardVesting(rewardVestingModule.address);
         await stakingPool.registerRewardToken(akro.address);
         await stakingPool.registerRewardToken(adel.address);
-
+        await rewardVestingModule.setDefaultEpochLength(REWARD_EPOCH_DURATION);
+        await rewardVestingModule.registerRewardToken(stakingPool.address, akro.address, 0);
+        await rewardVestingModule.registerRewardToken(stakingPool.address, adel.address, 0);
 
         //Prepare liquidity
         await akro.methods['mint(address,uint256)'](owner, web3.utils.toWei('1000000000'));
         await adel.methods['mint(address,uint256)'](owner, web3.utils.toWei('1000000000'));
+
+        //Prepare rewards
+        let rwrd = new BN(web3.utils.toWei('1000'));
+        await akro.approve(rewardVestingModule.address, web3.utils.toWei('100000000000000'));
+        await adel.approve(rewardVestingModule.address, web3.utils.toWei('100000000000000'));
+        for(let e = 0; e < 3; e++) {
+            await rewardVestingModule.addRewards(
+               [stakingPool.address, stakingPool.address],
+               [akro.address, adel.address],
+               [0,0],
+               [rwrd,rwrd.divn(e+1)]
+            );
+        }
 
         //Save snapshot
         //snap = await Snapshot.create(web3.currentProvider);
@@ -113,26 +129,27 @@ contract("StakingPool", async ([owner, user, ...otherAccounts]) => {
         expect(after.stakedTotal).to.be.bignumber.eq(before.stakedTotal.add(stakeAmount));
     });
 
-    it('should unstake own stake', async () => {
-        const before = {
-            userBalance: await akro.balanceOf(user),
-            stakingPoolBalance: await akro.balanceOf(stakingPool.address),
-            stakedTotal: await stakingPool.totalStakedFor(user),
-        }
-        let unstakeAmount = before.stakedTotal.divn(2);
+    // //Partial unstake is not supported
+    // it('should partially unstake own stake', async () => {
+    //     const before = {
+    //         userBalance: await akro.balanceOf(user),
+    //         stakingPoolBalance: await akro.balanceOf(stakingPool.address),
+    //         stakedTotal: await stakingPool.totalStakedFor(user),
+    //     }
+    //     let unstakeAmount = before.stakedTotal.divn(2);
 
-        await stakingPool.unstake(unstakeAmount, ZERO_DATA, {from:user});
+    //     await stakingPool.unstake(unstakeAmount, ZERO_DATA, {from:user});
 
-        const after = {
-            userBalance: await akro.balanceOf(user),
-            stakingPoolBalance: await akro.balanceOf(stakingPool.address),
-            stakedTotal: await stakingPool.totalStakedFor(user),
-        }
+    //     const after = {
+    //         userBalance: await akro.balanceOf(user),
+    //         stakingPoolBalance: await akro.balanceOf(stakingPool.address),
+    //         stakedTotal: await stakingPool.totalStakedFor(user),
+    //     }
 
-        expect(after.userBalance).to.be.bignumber.eq(before.userBalance.add(unstakeAmount));
-        expect(after.stakingPoolBalance).to.be.bignumber.eq(before.stakingPoolBalance.sub(unstakeAmount));
-        expect(after.stakedTotal).to.be.bignumber.eq(before.stakedTotal.sub(unstakeAmount));
-    });
+    //     expect(after.userBalance).to.be.bignumber.eq(before.userBalance.add(unstakeAmount));
+    //     expect(after.stakingPoolBalance).to.be.bignumber.eq(before.stakingPoolBalance.sub(unstakeAmount));
+    //     expect(after.stakedTotal).to.be.bignumber.eq(before.stakedTotal.sub(unstakeAmount));
+    // });
 
     it('should fully unstake own stake', async () => {
         const before = {
@@ -174,4 +191,89 @@ contract("StakingPool", async ([owner, user, ...otherAccounts]) => {
         expect(after.stakingPoolBalance).to.be.bignumber.eq(before.stakingPoolBalance.sub(before.stakedTotal));
         expect(after.stakedTotal).to.be.bignumber.eq(ZERO_BN);
     });
+
+    it('should receive rewards when staking for yourself', async () => {
+        let stakeAmount = w3random.interval(100, 500, 'ether');
+        await akro.allocateTo(user, stakeAmount);
+
+
+        await akro.approve(stakingPool.address, stakeAmount, {from:user});
+        await stakingPool.stake(stakeAmount, ZERO_DATA, {from:user});
+
+        const before = {
+            userBalanceAkro: await akro.balanceOf(user),
+            userBalanceAdel: await adel.balanceOf(user),
+            stakedTotal: await stakingPool.totalStakedFor(user),
+        }
+
+        await time.increase(REWARD_EPOCH_DURATION);
+        await stakingPool.claimRewardsFromVesting();
+
+        await stakingPool.withdrawRewards({from:user});
+
+        const afterWR = {
+            userBalanceAkro: await akro.balanceOf(user),
+            userBalanceAdel: await adel.balanceOf(user),
+            stakedTotal: await stakingPool.totalStakedFor(user),
+        }
+
+        expect(afterWR.userBalanceAkro).to.be.bignumber.gt(before.userBalanceAkro);
+        expect(afterWR.userBalanceAdel).to.be.bignumber.gt(before.userBalanceAdel);
+        expect(afterWR.stakedTotal).to.be.bignumber.eq(before.stakedTotal);
+
+        await stakingPool.unstakeAllUnlocked(ZERO_DATA, {from:user});
+
+        const afterU = {
+            userBalanceAkro: await akro.balanceOf(user),
+            userBalanceAdel: await adel.balanceOf(user),
+            stakedTotal: await stakingPool.totalStakedFor(user),
+        }
+
+        expect(afterU.userBalanceAkro).to.be.bignumber.gt(afterWR.userBalanceAkro.add(stakeAmount));
+        expect(afterU.userBalanceAdel).to.be.bignumber.eq(afterWR.userBalanceAdel);
+        expect(afterU.stakedTotal).to.be.bignumber.eq(ZERO_BN);
+    });
+
+    it('should receive rewards when staking for another user', async () => {
+        let stakeAmount = w3random.interval(100, 500, 'ether');
+        await akro.allocateTo(user, stakeAmount);
+
+
+        await akro.approve(stakingPool.address, stakeAmount, {from:user});
+        await stakingPool.stakeFor(otherAccounts[0], stakeAmount, ZERO_DATA, {from:user});
+
+        const before = {
+            userBalanceAkro: await akro.balanceOf(otherAccounts[0]),
+            userBalanceAdel: await adel.balanceOf(otherAccounts[0]),
+            stakedTotal: await stakingPool.totalStakedFor(otherAccounts[0]),
+        }
+
+        await time.increase(REWARD_EPOCH_DURATION);
+        await stakingPool.claimRewardsFromVesting();
+
+        await stakingPool.withdrawRewards({from:otherAccounts[0]});
+
+        const afterWR = {
+            userBalanceAkro: await akro.balanceOf(otherAccounts[0]),
+            userBalanceAdel: await adel.balanceOf(otherAccounts[0]),
+            stakedTotal: await stakingPool.totalStakedFor(otherAccounts[0]),
+        }
+
+        expect(afterWR.userBalanceAkro).to.be.bignumber.gt(before.userBalanceAkro);
+        expect(afterWR.userBalanceAdel).to.be.bignumber.gt(before.userBalanceAdel);
+        expect(afterWR.stakedTotal).to.be.bignumber.eq(before.stakedTotal);
+
+        await stakingPool.unstakeAllUnlocked(ZERO_DATA, {from:otherAccounts[0]});
+
+        const afterU = {
+            userBalanceAkro: await akro.balanceOf(otherAccounts[0]),
+            userBalanceAdel: await adel.balanceOf(otherAccounts[0]),
+            stakedTotal: await stakingPool.totalStakedFor(otherAccounts[0]),
+        }
+
+        expect(afterU.userBalanceAkro).to.be.bignumber.gt(afterWR.userBalanceAkro.add(stakeAmount));
+        expect(afterU.userBalanceAdel).to.be.bignumber.eq(afterWR.userBalanceAdel);
+        expect(afterU.stakedTotal).to.be.bignumber.eq(ZERO_BN);
+    });
+
 });
