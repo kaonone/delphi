74eb2616d10edaec5b37279475a6a63372fd983f
Pavel-Blaize <pavel.g@blaize.tech>
2020-11-26 22:45:02 +0200

    Echidna integration. Contracts flattering

--- contracts/test/echidna/TestVaultSavings.sol
+++ contracts/test/echidna/TestVaultSavings.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.5.12;
 import "../../modules/savings/VaultSavingsModule.sol";
 
 contract TestVaultSavings is VaultSavingsModule {
-    constructor() public {}
+    address public _pool = 0xb18E32002e1c13B0aDD0e0b860245BF429AABe09;
+    address public _deployer = 0x260dfB806B62baeBe10083142499f863528dD190;
+
+    constructor() public {
+        VaultSavingsModule.initialize(_pool);
+    }
     function echidna_vault_registered() public view returns(bool){
-        return registeredVaults.length > 0;
+        return owner() == _deployer;
     }
 
 }
